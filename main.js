b6bf2021aacd49ea8a0c60e51087e7bd420d8bf5
Siranjeevi <101230800+Siranjeeviv26@users.noreply.github.com>
2025-07-15 09:44:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,55 +5,115 @@ const scrollRevealOption = {
 };
 
 // header container
-ScrollReveal().reveal(".header__container h1", scrollRevealOption);
+ScrollReveal().reveal(".header_container h1", scrollRevealOption);
 
-ScrollReveal().reveal(".header__container h4", {
+ScrollReveal().reveal(".header_container h4", {
   ...scrollRevealOption,
   delay: 500,
 });
 
-ScrollReveal().reveal(".header__container .btn", {
+ScrollReveal().reveal(".header_container .btn", {
   ...scrollRevealOption,
   delay: 1000,
 });
 
 // about container
-ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
-ScrollReveal().reveal(".about__container .section__subheader", {
+ScrollReveal().reveal(".about_container .section_header", scrollRevealOption);
+ScrollReveal().reveal(".about_container .section_subheader", {
   ...scrollRevealOption,
   delay: 500,
 });
 
-ScrollReveal().reveal(".about__container .about__flex", {
+ScrollReveal().reveal(".about_container .about_flex", {
   ...scrollRevealOption,
   delay: 1000,
 });
 
-ScrollReveal().reveal(".about__container .btn", {
+ScrollReveal().reveal(".about_container .btn", {
   ...scrollRevealOption,
   delay: 1500,
 });
 
 // discover container
-ScrollReveal().reveal(".discover__card", {
+ScrollReveal().reveal(".discover_card", {
   ...scrollRevealOption,
   interval: 500,
 });
 
-ScrollReveal().reveal(".discover__card__content", {
+ScrollReveal().reveal(".discover_card_content", {
   ...scrollRevealOption,
   interval: 500,
   delay: 200,
 });
 
 // blogs container
-ScrollReveal().reveal(".blogs__card", {
+ScrollReveal().reveal(".blogs_card", {
   duration: 1000,
   interval: 400,
 });
 
 // journals container
-ScrollReveal().reveal(".journals__card", {
+ScrollReveal().reveal(".journals_card", {
   ...scrollRevealOption,
   interval: 400,
 });
+
+// const scrollRevealOption = {
+//   distance: "50px",
+//   origin: "bottom",
+//   duration: 1000,
+// };
+
+// // header container
+// ScrollReveal().reveal(".header__container h1", scrollRevealOption);
+
+// ScrollReveal().reveal(".header__container h4", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+
+// ScrollReveal().reveal(".header__container .btn", {
+//   ...scrollRevealOption,
+//   delay: 1000,
+// });
+
+// // about container
+// ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
+// ScrollReveal().reveal(".about__container .section__subheader", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+
+// ScrollReveal().reveal(".about__container .about__flex", {
+//   ...scrollRevealOption,
+//   delay: 1000,
+// });
+
+// ScrollReveal().reveal(".about__container .btn", {
+//   ...scrollRevealOption,
+//   delay: 1500,
+// });
+
+// // discover container
+// ScrollReveal().reveal(".discover__card", {
+//   ...scrollRevealOption,
+//   interval: 500,
+// });
+
+// ScrollReveal().reveal(".discover__card__content", {
+//   ...scrollRevealOption,
+//   interval: 500,
+//   delay: 200,
+// });
+
+// // blogs container
+// ScrollReveal().reveal(".blogs__card", {
+//   duration: 1000,
+//   interval: 400,
+// });
+
+// // journals container
+// ScrollReveal().reveal(".journals__card", {
+//   ...scrollRevealOption,
+//   interval: 400,
+// });
